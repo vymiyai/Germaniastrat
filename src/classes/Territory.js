c48ef45c465070e896e808158f1102a7bbbc2a50
Victor Yoshiaki Miyai <vymiyai@hotmail.com>
2014-08-20 16:04:52 +0000

    Correcao de comentarios.

--- src/classes/Territory.js
+++ src/classes/Territory.js
@@ -3,17 +3,19 @@
 // class that represents the data part of a territory to be presented in a map.
 var Territory = function( type )
 {
+    // initial values.
     this.timestamp = new Date().getTime();
     this.resources = { food: 0, ammo: 0, fuel: 0 };
     
     // initializes this instance with an instance description.
     this.init = function( territoryInstance )
     {
+        this.timestamp = territoryInstance.timestamp;
         this.resources = territoryInstance.resouces;
     };
     
     // updates the timestamp and resources in case they are changed.
-    // this method should be called everytime an interaction is made.
+    // this method should be called everytime an interaction is made or regularly from time to time.
     this.update = function()
     {
         // retrieve last timestamp.
@@ -27,7 +29,8 @@ var Territory = function( type )
         
         // calculate the amount of each resources that was produced or consumed during this time period.
         
-        // update units, conflicts and whatmore.
+        
+        // update battlegroups, attrition and whatmore.
         
         // update resources.
         this.resources = { food: 0, ammo: 0, fuel: 0 };
